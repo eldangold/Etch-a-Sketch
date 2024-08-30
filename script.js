b9539816602833df004b16b1812784b2ceb89296
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-08-30 18:14:57 +0200

    Finish first prototype with working hover effect

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ for (let x = 1; x <= 10; x++) {
  for (let i = 0; i < 10; i++) {
     rowContainer.appendChild(rowBlock.cloneNode(true));
 }
+
+for (block of document.querySelectorAll(".columnBlock")) {
+    block.addEventListener("mouseover", hoverEffect);
+   }
+
+function hoverEffect(block) {
+    block.target.classList.add("isHovered");
+
+}
