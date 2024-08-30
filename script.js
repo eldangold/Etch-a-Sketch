ced14964abfd84a6a255c55522ae190a7a2edc2e
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-08-30 15:02:23 +0200

    Create a working grid prototype

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-var div = document.createElement("div");
-div.className = "divSquare";
-let container = document.getElementById("container");
+let div = document.createElement("div");
+div.className = "rowBlock";
+let rowContainer = document.getElementById("rowContainer");
+let columnContainer = document.createElement("div");
+columnContainer.className = "columnContainer";
+let columnBlock = document.createElement("div");
+columnBlock.className = "columnBlock";
 
-for (let i = 0; i < 10; i++) {
- container.appendChild(div.cloneNode(true));
+
+for (let x = 1; x <= 100; x++) {
+    columnContainer.appendChild(columnBlock.cloneNode(true));
+ } 
+
+for (let x = 1; x <= 100; x++) {
+    div.appendChild(columnContainer);
+ }
+
+
+ 
+ for (let i = 0; i < 100; i++) {
+    rowContainer.appendChild(div.cloneNode(true));
 }
