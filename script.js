2fdce92d54f11d3222d7aef1bfbad6613664d82e
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-08-30 19:34:45 +0200

    Make a function to specify grid size

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ let rowContainer = document.getElementById("rowContainer");
 let columnBlock = document.createElement("div");
 columnBlock.className = "columnBlock";
 
-for (let x = 1; x <= 10; x++) {
-    rowBlock.appendChild(columnBlock.cloneNode(true));
- }
- 
- for (let i = 0; i < 10; i++) {
-    rowContainer.appendChild(rowBlock.cloneNode(true));
+function makeGrid(size) {
+    for (let x = 1; x <= size; x++) {
+        rowBlock.appendChild(columnBlock.cloneNode(true));
+    }
+    
+    for (let i = 0; i < size; i++) {
+        rowContainer.appendChild(rowBlock.cloneNode(true));
+    }
+
+    for (block of document.querySelectorAll(".columnBlock")) {
+        block.addEventListener("mouseover", hoverEffect);
+    }
 }
 
-for (block of document.querySelectorAll(".columnBlock")) {
-    block.addEventListener("mouseover", hoverEffect);
-   }
-
-
-   let hoverColor = () =>  { 
+let hoverColor = () =>  { 
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     return `#${randomColor}`
 }
-
-
 
 function hoverEffect(block) {
     //block.target.classList.add("isHovered");
     block.target.style.backgroundColor = hoverColor();
 
 }
+
+makeGrid(10);
 
